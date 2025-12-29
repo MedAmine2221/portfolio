7505dab256ce2f3fcb83b9d3042ca1412685ad66
MedAmine2221 <lazregamine258@gmail.com>
2025-12-29 17:21:33 +0100

    add page transition animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import PageTransition from "@/components/page-transition";
 
 export const metadata: Metadata = {
   title: {
@@ -45,7 +46,9 @@ export default function RootLayout({
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
+              <PageTransition>
+                {children}
+              </PageTransition>
             </main>
             <footer className="w-full flex items-center justify-center py-3">
               <Link
