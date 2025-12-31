5707f233878782d41243b54f01ada3172d1a9d0f
MedAmine2221 <lazregamine258@gmail.com>
2025-12-31 19:25:43 +0100

    fix lint

--- components/redux-provider.tsx
+++ components/redux-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 import * as React from "react";
 import { Provider } from "react-redux";
-
 import { store } from "@/redux/store";
 
 export interface ReduxProvidersProps {
@@ -9,5 +8,5 @@ export interface ReduxProvidersProps {
 }
 
 export function ReduxProviders({ children }: ReduxProvidersProps) {
-  return <Provider store={store}>{children}</Provider>;
+  return <Provider store={store}>{children}</Provider>
 }
