e80c781bafa3588f2c8923fc48a8bc7c4a262a99
MedAmine2221 <lazregamine258@gmail.com>
2026-01-07 10:42:48 +0100

    change sit description

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Mohamed Amine LAZREG",
-  description: "Make beautiful websites regardless of your design experience.",
+  description: "Mohamed Amine's portfolio",
   navItems: [
     {
       label: "Home",
